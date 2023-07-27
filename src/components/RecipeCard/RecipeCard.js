603bebe0d9d2f3807187fa206e2de75551aab3f1
klanti117 <klantyislam@yahoo.com>
2023-07-27 10:36:25 -0400

    remove card with btn click

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { originalRecipes } from '../RecipeHolder/RecipeHolder'
+
+function removeList (id){
+    alert(`Calling from RecipeHolder: card ${id} clicked`)
+    const newRecipes = originalRecipes.filter((item)=>item.id !== id)
+    return newRecipes
+}
 
 const RecipeCard = ({recipe}) => {
   return (
     <div style={{border:'2px solid black'}}>
         <h3>{recipe.name}</h3>
         <p>{recipe.ingredient}</p>
-        <button onClick={()=> alert(`card ${recipe.id} clicked`)}>Not tonight</button>
+        <button onClick={()=> removeList(recipe.id)}>Not tonight</button>
     </div>
   )
 }
--- src/components/RecipeHolder/RecipeHolder.js
+++ src/components/RecipeHolder/RecipeHolder.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import { useState }from 'react'
-import RecipeCard from '../RecipeCard/RecipeCard'
-
-const originalRecipes = [
-    { id: 1, name: 'Chicken Curry', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
-    { id: 2, name: 'Spicy Salmon', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
-    { id: 3, name: 'Meatloaf', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
-    { id: 4, name: 'Pinapple Fried Rice', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
-    { id: 5, name: 'Tuna Salad', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
-]
+import React, { useState } from 'react'
 
 const containerStyles = {
     padding: '1rem',
     display:'grid',
     gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
 }
-
+  
 const RecipeHolder = () => {
-  const [recipes, setRecipes] = useState(originalRecipes)  
-  const recipeList = recipes.map((recipe)=>(
-    <RecipeCard key={recipe.id} recipe={recipe}/>
-  ))  
-  return (
-    <div style={containerStyles}>
-        {recipeList}
-    </div>
-  )
+    const [recipes, setRecipes] = useState([
+        { id: 1, name: 'Chicken Curry', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
+        { id: 2, name: 'Spicy Salmon', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
+        { id: 3, name: 'Meatloaf', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
+        { id: 4, name: 'Pinapple Fried Rice', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
+        { id: 5, name: 'Tuna Salad', ingredient: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam' },
+    ])  
+
+    const removeList = (id) => {
+        // alert(`Calling from RecipeHolder: card ${id} clicked`)
+        const newRecipes = recipes.filter((item)=>item.id !== id)
+        setRecipes(newRecipes)
+    }
+
+    const recipeList = recipes.map((recipe)=>(
+        <div style={{border:'2px solid black'}}>
+            <h3>{recipe.name}</h3>
+            <p>{recipe.ingredient}</p>
+            <button onClick={()=> removeList(recipe.id)}>Not tonight</button>
+        </div>
+    )) 
+
+    return (
+        <div style={containerStyles}>
+            {recipeList}
+        </div>
+    )
 }
 
 export default RecipeHolder
